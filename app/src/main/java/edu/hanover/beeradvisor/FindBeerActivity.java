e14fc81cf0dfb98c956474fa461b0edc41ecdb64
Theresa Wilson <theresaawilson@gmail.com>
2018-05-15 18:36:41 -0400

    Add onClickFindBeer to FindBeerActivity and onClick attribute to Button in layout.

--- app/src/main/java/edu/hanover/beeradvisor/FindBeerActivity.java
+++ app/src/main/java/edu/hanover/beeradvisor/FindBeerActivity.java
@@ -2,6 +2,9 @@ package edu.hanover.beeradvisor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
@@ -9,5 +12,21 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // Call when the button gets clicked
+    public void onClickFindBeer(View view) {
+
+        // Get a reference to the TextView
+        TextView brands = findViewById(R.id.brands);
+
+        // Get a reference to the Spinner
+        Spinner color = findViewById(R.id.color);
+
+        // Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        // Display the selected item
+        brands.setText(beerType);
     }
 }
